7289e35422d3509ce4f22256d8fc5c3d2da26911
lacleung <lacleung@gmail.com>
2023-03-29 15:44:40 -0700

    DEV: add cli input

--- tests/fizzbuzz/fizzbuzz.sh
+++ tests/fizzbuzz/fizzbuzz.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-read -p "Enter start of range: " start_seq
-read -p "Enter end of range: " end_seq
+if [[ "$#" != 2 ]] ; then
+    read -p "Enter start of range: " start_seq
+    read -p "Enter end of range: " end_seq
+else
+    start_seq="${1}"
+    end_seq="${2}"
+fi
 
 for i in $(seq ${start_seq} ${end_seq}) ; do
     result=''
